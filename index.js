ff46a022bb8c655495b21e2b2bcb6e87ea956139
brincolnar <brincolnar2000@gmail.com>
2020-11-23 11:10:39 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const apiKey = "AIzaSyAG6cMYtyuVzQeuq_f1U94gtuBbWpx3d4k";
+const https = require("https");
+
+const getVideoByKeyword = (keyword) => {
+  // youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=The%20Weekend&key=AIzaSyAG6cMYtyuVzQeuq_f1U94gtuBbWpx3d4k
+
+  const req = https.get(
+    "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=The%20Weekend&key=AIzaSyAG6cMYtyuVzQeuq_f1U94gtuBbWpx3d4k",
+    (res) => {
+      console.log(`statusCode: ${res.statusCode}`);
+
+      res.on("data", (data) => {
+        process.stdout.write(data);
+      });
+    }
+  );
+};
+
+getVideoByKeyword("");
