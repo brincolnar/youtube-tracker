86e18ebfc411875b9819bc5e42abbda5f0d679d3
brincolnar <67594316+brincolnar@users.noreply.github.com>
2020-11-23 11:13:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const getVideoByKeyword = (keyword) => {
   // youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=The%20Weekend&key=AIzaSyAG6cMYtyuVzQeuq_f1U94gtuBbWpx3d4k
 
   const req = https.get(
-    "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=The%20Weekend&key=AIzaSyAG6cMYtyuVzQeuq_f1U94gtuBbWpx3d4k",
+    "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=The%20Weekend&key=[YOUR_API_KEY]",
     (res) => {
       console.log(`statusCode: ${res.statusCode}`);
 
