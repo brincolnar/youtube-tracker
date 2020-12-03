7e4571d958ab4b0f6b93c97585a4c3cd825aca9b
brincolnar <brincolnar2000@gmail.com>
2020-12-04 00:18:59 +0100

    Fixed week range problems

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ const apiKey = "AIzaSyAG6cMYtyuVzQeuq_f1U94gtuBbWpx3d4k";
 
 // get video duration by id
 const getVideoDurationById = (id) => {
-
   https.get(
     `https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&id=${id}&key=${apiKey}`,
     (res) => {
-
       res.on("data", (data) => {
         const json = JSON.parse(data);
 
@@ -20,39 +18,36 @@ const getVideoDurationById = (id) => {
 
         // Get duration in seconds);
         let durationInSeconds = 0;
-        let unit = 'S';
+        let unit = "S";
         let exponent = 0;
 
-        for(let i = duration.length-1; i > -1; i--) {
-
-          let current = parseInt(duration.charAt(i)); // 
-          if(!isNaN(current)) { // current is numerical
+        for (let i = duration.length - 1; i > -1; i--) {
+          let current = parseInt(duration.charAt(i)); //
+          if (!isNaN(current)) {
+            // current is numerical
             switch (unit) {
-              case 'S':
-
-                if(exponent == 0) {
+              case "S":
+                if (exponent == 0) {
                   durationInSeconds += current; // add seconds
                   exponent++;
                 } else {
-                  durationInSeconds += 10*current;
+                  durationInSeconds += 10 * current;
                 }
-                
+
                 break;
 
-              case 'M':
-
-                if(exponent == 0) {
+              case "M":
+                if (exponent == 0) {
                   durationInSeconds += current * 60; // add minutes
                   exponent++;
                 } else {
                   durationInSeconds += current * 60 * 10; // add minutes
                 }
-                
+
                 break;
 
-              case 'H':
-
-                if(exponent == 0) {
+              case "H":
+                if (exponent == 0) {
                   durationInSeconds += current * 60 * 60; // add hours
                   exponent++;
                 } else {
@@ -64,15 +59,14 @@ const getVideoDurationById = (id) => {
               default:
                 break;
             }
-          } else { // we're dealing with a unit marker
+          } else {
+            // we're dealing with a unit marker
             unit = duration.charAt(i);
             exponent = 0;
           }
-
         }
 
         console.log(durationInSeconds + "s");
-
       });
     }
   );
@@ -85,9 +79,8 @@ const totalTime = 0; // in seconds
 const data = JSON.parse(fs.readFileSync("zgodovina_ogledov.json"));
 
 const getVideosLatestWeek = () => {
-
   let latestDate = data[0];
-  // console.log(parseISOString(latestDate.time).getDay()); 
+  // console.log(parseISOString(latestDate.time).getDay());
 
   // get videos from this week
   /*
@@ -98,53 +91,64 @@ const getVideosLatestWeek = () => {
   */
 
   let weekNumber = 1; // keeps track of the current week - (week is defined from Monday to Sunday -- inclusive)
-  let firstDateInWeek = parseISOString(data[data.length-1].time); // first date in current week (String -> Date)
-  console.log("firstDateInWeek: " + firstDateInWeek);
+  let firstDateInWeek = parseISOString(data[data.length - 1].time); // first date in current week (String -> Date)
+  // console.log("firstDateInWeek: " + firstDateInWeek);
 
-  let daysLater = 10;
-  let testDate= firstDateInWeek.addDays(daysLater);
-  console.log("firstDateInWeek " + daysLater + " days later: " + testDate);
+  let daysLater = 379;
+  let testDate = firstDateInWeek.addDays(daysLater);
 
-  console.log("withinWeek():  " + withinWeek(testDate, testDate)); // testing method
+  console.log("given date:  " + new Date("December 31 2020"));
+  withinWeek(new Date("December 31 2020"), testDate); // testing method
   /*
   for (let index = data.length; index > -1; index--) {
     const element = data[index];
     // console.log(element);
   }
   */
-}
+};
 
-const parseISOString = s => {
+const parseISOString = (s) => {
   let b = s.split(/\D+/);
   return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
-}
-// TO-DO: fix withinWeek(), so that it returns the corresponding range correctly (for Sundays it goes forward and starts with Mondays)
-const withinWeek = (firstDateInWeek, current) => { // testing whether current (date) is in the week that was started with firstDateInWeek date
+};
 
-  let first = firstDateInWeek.getDate() - firstDateInWeek.getDay() + 1; // determine first and last day in this week
-  /*
-  if(firstDateInWeek.getDay() == 0) { // Sunday
-    first = firstDateInWeek.getDate() - 6;
-  }
-  */
- 
-  let last = first + 6;
+// TO-DO: check if within works as expected
+const withinWeek = (firstDateInWeek, current) => {
+  // testing whether current (date) is in the week that was started with firstDateInWeek date
 
-  let firstDay = new Date(firstDateInWeek.setDate(first)); // create Date objects
-  console.log("firstDay:  " + firstDay);
+  let firstDay = startOfWeek(firstDateInWeek); // create Date objects
+  let lastDay = endOfWeek(firstDateInWeek);
 
-  let lastDay = new Date(firstDateInWeek.setDate(last));
-  console.log("lastDay:  " + lastDay);
+  console.log(
+    "In week from " + firstDay.toString() + " to " + lastDay.toString() + "."
+  );
 
-  let within = (firstDay.getTime() <= current.getTime() && current.getTime() <= lastDay.getTime()) ? true : false;
+  let within =
+    firstDay.getTime() <= current.getTime() &&
+    current.getTime() <= lastDay.getTime()
+      ? true
+      : false;
   return within;
-}
+};
 
+// gets the date that corresponds to the end of the week
+const endOfWeek = (date) => {
+  var diff = date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1);
+
+  return new Date(date.setDate(diff + 6));
+};
+
+// gets the date that corresponds to the start of the week
+const startOfWeek = (date) => {
+  var diff = date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1);
+
+  return new Date(date.setDate(diff));
+};
 // for quickly testing
-Date.prototype.addDays = function(days) {
+Date.prototype.addDays = function (days) {
   var date = new Date(this.valueOf());
   date.setDate(date.getDate() + days);
   return date;
-}
+};
 
 getVideosLatestWeek();
